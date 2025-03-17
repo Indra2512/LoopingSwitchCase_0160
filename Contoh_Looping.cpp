df23401a29d4ba3076d6326a630d69331edf0abd
Indra2512 <mononeon1225@gmail.com>
2025-03-17 15:28:43 +0700

    menambahkan variabel

---
+++ Contoh_Looping.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDO = 0;
+
+    //perulangan dengan for
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n" = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
